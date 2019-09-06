83bcacc8acdcd3d73831f9925db8f504f51ea9b4
lrpkfz@163.com <lrpkfz@163.com>
2019-09-06 15:41:19 +0800

    测试吐司一条消息

--- app/src/main/java/com/yumeng/mytestgit/MainActivity.java
+++ app/src/main/java/com/yumeng/mytestgit/MainActivity.java
@@ -3,6 +3,7 @@ package com.yumeng.mytestgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "你好我是刚刚创建的项目", Toast.LENGTH_SHORT).show();
     }
 }
